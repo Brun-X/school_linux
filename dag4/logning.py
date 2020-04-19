9b156451976e025ade31217ec89d711c10776929
søren brun <sbrun8600@gmail.com>
2020-04-19 07:28:25 +0200

    Added comments

--- dag4/logning.py
+++ dag4/logning.py
@@ -1,68 +1,65 @@
-from sense_hat import SenseHat as shat
-import time
-import keyboard
+from sense_hat import SenseHat as shat 																				#Library til sensehat
+import time																											#Library til bl. a. slepp funktion
+import _thread																										#Library til at lave tråde
 
-import _thread
+global flag																											#Global variabel til at signalerer mellem tråde
+global terminate																									#Global variabel til at signalerer mellem tråde
 
-global flag
-global terminate
+flag = 0																											#Global variabel initialiseres
+terminate = 0																										#Global variabel initialiseres
 
-flag = 0
-terminate = 0
+sh = shat()																											#Opretter et sense hat objekt
 
-sh = shat()
+file = "log.csv"																									#Opretter string-variabel med navnet på filen der logges til 
 
-file = "log.csv"
-
-def write_labels():
+def write_labels():																									#Funktion der skriver "labels" til log-filen
 	with open(file, 'a+') as f:
-		f.write('Temperature' + ',' + 'Humidity' + ',' + 'Pressure' + '\n')
+		f.write('Temperature' + ',' + 'Humidity' + ',' + 'Pressure' + '\n')											#Sammensætter linje i csv-filen
 
-def write_empty_line():
+def write_empty_line():																								#Funktion der skriver en tom linje i csv-filen
 	with open(file, 'a+') as f:
 		f.write('\n')
 
-def append_to_file(data):
-	with open(file, 'a+') as f:
-		entry = str(round(data['t'],2)) + ',' + str(round(data['h'], 0)) + ',' + str(round(data['p'], 2)) + '\n'
-		f.write(entry)
+def append_to_file(data):																							#Funktion der skriver data til csv-filen data er en dictionary
+	with open(file, 'a+') as f:																						#Åbner filen med with, som selv sørger for at lukke file handle-
+																													#når scope er slut
+		entry = str(round(data['t'],2)) + ',' + str(round(data['h'])) + ',' + str(round(data['p'], 2)) + '\n'	#Laver string sammensat af data. Adskilt af kommaer og slutter med new line 
+		f.write(entry)																								#Skriver til filen
 
-def get_sensor_data():
+def get_sensor_data():																								#Funtion der tager data fra sensorerne
 	t = sh.get_temperature()
 	h = sh.get_humidity()
 	p = sh.get_pressure()
-	return {'t' : t, 'h' : h, 'p' : p}
+	return {'t' : t, 'h' : h, 'p' : p}																				#Returnerer en dictionary med sensorværdierne
 
+def logger_function():																								#Funktion der køres i seperat tråd. Logger data til csv-filen hvert sekund
+	print("Logging... Press enter to stop")																			
+	global flag																										#Gør global variabel tilgængelig for funktion
+	while flag == 0:																								#Mens flag er nul 
+		append_to_file(get_sensor_data())																			#Skriv sensor-data til fil
+		time.sleep(1)																								#Vent 1 sekund
+	print("Logging stopped!")																						
+	global terminate																								#Gør variabel tilgængelig for funktion
+	terminate = 1																									#Sætter terminate variablen til 1
+	return 																											#returnér fra funktion. (Nødvendigt for .join() for at stoppe tråden)
 
+def keyboard_function():																							#Funktion der køres i seperat tråd. Venter på der trykkes enter på keyboarded
+	input()																											#Venter på enter
+	global flag																										#Gør global variabel tilgængelig for tråd
+	flag = 1																										
+	return 																											#returnér fra funktion. (Nødvendigt for .join() for at stoppe tråden)
 
-def logger_function():
-	print("Logging... Press enter to stop")
-	global flag
-	while flag == 0:
-		append_to_file(get_sensor_data())
-		time.sleep(1)
-	print("Logging stopped!")
-	global terminate
-	terminate = 1
-	return
+write_labels()																										#Programmet starter med at skrive "labels" i csv-filen
 
-def keyboard_function():
-	input()
-	global flag
-	flag = 1
-	return
+try:																												#Try er en slags conditional konstruktion der "prøver at køre kode"
+																													#Men springer til except hvis der opstår en fejl
+	_thread.start_new_thread(logger_function, ())																	#Starter ny tråd som kører funktionen logger_function med 0 inputparametre
+	_thread.start_new_thread(keyboard_function, ())																	#Starter ny tråd som kører funktionen keyboard_function med 0 inputparametre
 
-write_labels()
+except Exception as e:																								#Hvis koden i try fejler
+	print(str(e))																									#Printer fejlen som string 
 
-try:
-	t1 = _thread.start_new_thread(logger_function, ())
-	time.sleep(0.5)
-	_thread.start_new_thread(keyboard_function, ())
+while terminate == 0:																								#Mens terminate = 0. (mens enter ikke trykket på endnu)
+	pass																											#Gør ingen ting da seperate tråde laver arbejdet.
 
-except Exception as e:
-	print("error: " + str(e))
-
-while terminate == 0:
-	pass
-
-write_empty_line()
+write_empty_line()																									#Når programmet afsluttes skrives en tom linje til csv-filen
